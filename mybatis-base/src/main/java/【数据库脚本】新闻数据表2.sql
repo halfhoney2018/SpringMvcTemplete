39a57702e1adef41d16477516ba5428b4245d4ee
LeeHappy <happy@git.com>
2019-06-24 15:58:40 +0800

    动态SQL简介

--- mybatis-base/src/main/java/【数据库脚本】新闻数据表2.sql
+++
@@ -1,9 +0,0 @@
-DROP DATABASE IF EXISTS yootk ;
-CREATE DATABASE yootk CHARACTER SET UTF8;
-USE yootk ;
-CREATE TABLE t_news(
-  c_nid   BIGINT    AUTO_INCREMENT ,
-  c_title VARCHAR(50) ,
-  c_content TEXT ,
-  CONSTRAINT pk_nid PRIMARY KEY(c_nid)
-) engine=innodb ;
